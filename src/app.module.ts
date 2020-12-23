1c69c68aa56a68e4b96e3bbf49866c3976fdf89a
Petr Juna <petr.juna@gmail.com>
2020-12-23 20:04:02 +0100

    fix:  initial settings for webhook

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { InvoiceModule } from './invoice/invoice.module';
+import { StripeModule } from './stripe/stripe.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [InvoiceModule, StripeModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
